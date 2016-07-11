192ed80374d5d781a5dab36be232e9a53d562c9c
chrissata <767720369@qq.com>
2016-07-11 23:20:42 +0800

    Add tastypie config & config admin

--- pjhr/erp/admin.py
+++ pjhr/erp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from erp.models import Employee, SystemArg, Company, Project, SalaryStandard, SalaryTemplate, Attendance
+
+admin.site.register(Employee)
+admin.site.register(SystemArg)
+admin.site.register(Company)
+admin.site.register(Project)
+admin.site.register(SalaryTemplate)
+admin.site.register(SalaryStandard)
+admin.site.register(Attendance)
+
